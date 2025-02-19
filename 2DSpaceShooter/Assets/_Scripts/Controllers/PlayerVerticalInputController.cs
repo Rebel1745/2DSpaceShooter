150e91498764c80d63d47d45c0b0848f622ac2b1
Rebel1745 <31040719+Rebel1745@users.noreply.github.com>
2025-02-19 16:46:03 +0000

    Movement bounds, projectile scriptable object
    Limited the X and Y movement of the ship with configurable bounds.
    
    Made the missile into a scriptable object to hold the data

--- 2DSpaceShooter/Assets/_Scripts/Controllers/PlayerVerticalInputController.cs
+++ 2DSpaceShooter/Assets/_Scripts/Controllers/PlayerVerticalInputController.cs
@@ -4,37 +4,94 @@ using UnityEngine.InputSystem;
 
 public class PlayerVerticalInputController : MonoBehaviour
 {
-    Vector2 _moveInput;
-    [SerializeField] float _moveSpeed = 5f;
+    float _cameraOrthographicSize;
+    [SerializeField] Vector2 _axisBoundsX;
+    [SerializeField] Vector2 _axisBoundsY;
+    int _normInputX, _normInputY;
+    Vector2Int _moveInput;
+    [SerializeField] Vector2 _moveSpeed = new(8f, 5f);
     [SerializeField] Transform _missileSpawnPoint;
     [SerializeField] Transform _missleHolder;
-    [SerializeField] GameObject _missilePrefab;
-    [SerializeField] float _missileSpeed = 5f;
-    [SerializeField] float _timeUntilMissileDestroyed = 2f;
+    [SerializeField] ProjectileSO _projectile;
+    float _currentProjectileCooldown;
+    bool _fireMissile = false;
+
+    void Awake()
+    {
+        _cameraOrthographicSize = Camera.main.orthographicSize;
+    }
 
     public void Movement(InputAction.CallbackContext context)
     {
-        _moveInput = context.ReadValue<Vector2>().normalized;
+        _normInputX = (int)(context.ReadValue<Vector2>() * Vector2.right).normalized.x;
+        _normInputY = (int)(context.ReadValue<Vector2>() * Vector2.up).normalized.y;
+        _moveInput = new Vector2Int(_normInputX, _normInputY);
     }
 
     public void Missile(InputAction.CallbackContext context)
     {
         if (context.started)
         {
-            FireMissile();
+            _fireMissile = true;
+        }
+        else if (context.canceled)
+        {
+            _fireMissile = false;
         }
     }
 
     private void FireMissile()
     {
-        GameObject newMissile = Instantiate(_missilePrefab, _missileSpawnPoint.position, _missileSpawnPoint.rotation, _missleHolder);
-        newMissile.GetComponent<Missile>().SetMissileSpeed(_missileSpeed);
-        newMissile.GetComponent<DestroyAfterTime>().SetDestroyAfterTime(_timeUntilMissileDestroyed);
+        _currentProjectileCooldown = _projectile.Cooldown;
+        GameObject newMissile = Instantiate(_projectile.ProjectilePrefab, _missileSpawnPoint.position, _missileSpawnPoint.rotation, _missleHolder);
+        newMissile.GetComponent<Missile>().SetMissileSpeed(_projectile.ProjectileSpeed);
+        newMissile.GetComponent<DestroyAfterTime>().SetDestroyAfterTime(_projectile.TimeUntilProjectileDestroyed);
     }
 
     private void Update()
     {
+        DoMovement();
+        DoCooldowns();
+        DoAttacks();
+    }
+
+    private void DoAttacks()
+    {
+        if ((_fireMissile || _projectile.AutoFire) && _currentProjectileCooldown <= 0f)
+        {
+            FireMissile();
+        }
+    }
+
+    private void DoCooldowns()
+    {
+        _currentProjectileCooldown -= Time.deltaTime;
+    }
+
+    private void DoMovement()
+    {
         Vector2 moveTo = _moveInput * _moveSpeed * Time.deltaTime;
         transform.Translate(moveTo.x, moveTo.y, 0f);
+
+        float widthHeighRatio = (float)Screen.width / (float)Screen.height;
+        float adjustedScreenWidth = _cameraOrthographicSize * widthHeighRatio;
+
+        // limit movement to screen size
+        if (transform.position.y > _cameraOrthographicSize - _axisBoundsY.y)
+        {
+            transform.position = new Vector3(transform.position.x, _cameraOrthographicSize - _axisBoundsY.y, 0f);
+        }
+        else if (transform.position.y < -_cameraOrthographicSize + _axisBoundsY.x)
+        {
+            transform.position = new Vector3(transform.position.x, -_cameraOrthographicSize + _axisBoundsY.x, 0f);
+        }
+        if (transform.position.x > adjustedScreenWidth - _axisBoundsX.y)
+        {
+            transform.position = new Vector3(adjustedScreenWidth - _axisBoundsX.y, transform.position.y, 0f);
+        }
+        else if (transform.position.x < -adjustedScreenWidth + _axisBoundsX.x)
+        {
+            transform.position = new Vector3(-adjustedScreenWidth + _axisBoundsX.x, transform.position.y, 0f);
+        }
     }
 }
---
+++ 2DSpaceShooter/Assets/_Scripts/Projectiles/ProjectileSO.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewProjectile", menuName = "Projectile")]
+public class ProjectileSO : ScriptableObject
+{
+
+    public GameObject ProjectilePrefab;
+    public float ProjectileSpeed = 5f;
+    public float TimeUntilProjectileDestroyed = 2f;
+    public float Cooldown = 0.5f;
+    public bool AutoFire = false;
+}
