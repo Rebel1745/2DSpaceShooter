096af28673c03efcd4c82dd9b6e1a4343cd7d48e
Rebel1745 <31040719+Rebel1745@users.noreply.github.com>
2025-05-01 16:26:41 +0100

    Bombs work baby!
    Sorted the muddled, silly code written yesterday into a working bomb firing and exploding system

--- 2DSpaceShooter/Assets/_Scripts/Entities/Projectiles/Bomb.cs
+++ 2DSpaceShooter/Assets/_Scripts/Entities/Projectiles/Bomb.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
-using UnityEngine.Events;
 
 public class Bomb : Entity, IDestroyable
 {
     private WeaponBomb _bombData;
     private float _bombSpawnTime;
+    private Collider2D[] _enemyColliders;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (HasComeOnscreenYet)
+        {
+            if (_bombData.DestroyWhenOffscreenDistance != 0f && CheckIfOffscreenByAmount(_bombData.DestroyWhenOffscreenDistance))
+                QuietDestroy();
+        }
+    }
 
     public void SetupBomb(WeaponBomb bombData)
     {
@@ -14,22 +25,32 @@ public class Bomb : Entity, IDestroyable
         _bombSpawnTime = Time.time;
     }
 
-    public bool ExplodeBomb(bool loud)
+    public bool ExplodeBomb()
     {
-        // if the bomb has not been alive long enough to trigger, bail
-        if (_bombSpawnTime + _bombData.MinimumTimeBeforeBombCanTrigger < Time.time) return false;
+        // find all of the objects inside the blast radius
+        _enemyColliders = Physics2D.OverlapCircleAll(transform.position, _bombData.ExplosionRadius, _bombData.WhatIsEnemy);
+
+        if (_enemyColliders.Length > 0)
+        {
+            foreach (Collider2D collider in _enemyColliders)
+            {
+                if (collider.transform.TryGetComponent<IDamageable>(out IDamageable obj))
+                {
+                    obj.TakeDamage(_bombData.Damage);
+                }
+            }
+        }
 
         // time to explode
-        if (loud)
-            DestroyObject();
-        else
-            QuietDestroy();
+        DestroyObject();
 
         return true;
     }
 
     public void DestroyObject()
     {
+        _bombData.MarkBombExploded();
+
         if (_bombData.DestructionParticles)
             ObjectPoolManager.SpawnObject(_bombData.DestructionParticles, transform.position, Quaternion.identity, ObjectPoolManager.POOL_TYPE.ParticleSystem);
 
@@ -38,6 +59,8 @@ public class Bomb : Entity, IDestroyable
 
     public void QuietDestroy()
     {
+        _bombData.MarkBombExploded();
+
         ObjectPoolManager.ReturnObjectToPool(gameObject);
     }
 }
--- 2DSpaceShooter/Assets/_Scripts/ScriptableObjects/Weapons/Bomb/WeaponBomb.cs
+++ 2DSpaceShooter/Assets/_Scripts/ScriptableObjects/Weapons/Bomb/WeaponBomb.cs
@@ -6,60 +6,68 @@ using UnityEngine;
 public class WeaponBomb : WeaponBase
 {
     public float CooldownBetweenFiring = 2f;
-    public int MaximumBombsOnscreen = 1;
     public float ProjectileSpeed = 2f;
     public bool IsPlayerProjectile;
     public GameObject DestructionParticles;
     public float DestroyWhenOffscreenDistance = 1f;
     public float MinimumTimeBeforeBombCanTrigger = 1f;
     public float ExplosionRadius;
-    private Dictionary<Bomb, float> _currentBombs;
+    private Bomb _currentBomb;
+    private float _earliestExplosionTime;
+    private float _nextPossibleFireTime;
 
     public override void LoadWeapon(Transform thisTransform, Transform[] spawnPoints)
     {
         base.LoadWeapon(thisTransform, spawnPoints);
 
-        _currentBombs = new Dictionary<Bomb, float>();
+        _currentBomb = null;
+        _timeWeaponLastFired = 0;
+        _earliestExplosionTime = 0;
+        _nextPossibleFireTime = 0;
     }
 
     public override bool CanAttackBeStarted()
     {
-        if (_currentBombs.Count == MaximumBombsOnscreen) return false;
-        if (_timeWeaponLastFired + CooldownBetweenFiring < Time.time) return false;
-
         return true;
     }
 
     public override bool IsWeaponAttacking()
     {
-        return _isWeaponAttacking;
+        return false;
     }
 
     public override void StartAttack()
     {
-        /*if (_currentBomb == null)
+        if (_currentBomb == null && _nextPossibleFireTime < Time.time)
+        {
             SpawnBomb();
-        else
-            ExplodeBomb();*/
+            return;
+        }
+
+        if (_currentBomb != null && Time.time > _earliestExplosionTime)
+        {
+            _currentBomb.ExplodeBomb();
+        }
     }
 
     public override void UpdateAttack()
     {
-        throw new NotImplementedException();
+        Debug.LogError("This should never be called for the bomb");
     }
 
-    private void ExplodeBomb()
+    public void MarkBombExploded()
     {
-        /*if (_currentBomb.ExplodeBomb(true))
-        {
-            _currentBomb = null;
-        }*/
+        _currentBomb = null;
+        _timeWeaponLastFired = Time.time;
+        _nextPossibleFireTime = Time.time + CooldownBetweenFiring;
     }
 
     private void SpawnBomb()
     {
-        /*GameObject newBomb = ObjectPoolManager.SpawnObject(WeaponPrefab, _spawnPoints[0].position, Quaternion.identity, ObjectPoolManager.POOL_TYPE.Projectile);
+        _earliestExplosionTime = Time.time + MinimumTimeBeforeBombCanTrigger;
+
+        GameObject newBomb = ObjectPoolManager.SpawnObject(WeaponPrefab, _spawnPoints[0].position, Quaternion.identity, ObjectPoolManager.POOL_TYPE.Projectile);
         _currentBomb = newBomb.GetComponent<Bomb>();
-        _currentBomb.SetupBomb(this);*/
+        _currentBomb.SetupBomb(this);
     }
 }
